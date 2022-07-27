6ebba79d10c5d60038aea1a4e6a9dd0b7ba7b1f7
ancs214 <ancs214@gmail.com>
2022-07-27 09:30:14 -0400

    created approval card component

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,20 +1,18 @@
-import { faker } from '@faker-js/faker'
-
-const CommentDetail = (props) => {
+const CommentDetail = ({ author, timeAgo, text, avatar }) => {
   //   console.log(props)
   return (
     <div className='comment'>
       <a href='/' className='avatar'>
-        <img alt='avatar' src={faker.image.avatar()} />
+        <img alt='avatar' src={avatar} />
       </a>
       <div className='content'>
         <a href='/' className='author'>
-          {props.author}
+          {author}
         </a>
         <div className='metadata'>
-          <span className='date'>Today at 1800</span>
+          <span className='date'>{timeAgo}</span>
         </div>
-        <div className='text'>Nice Blog Post</div>
+        <div className='text'>{text}</div>
       </div>
     </div>
   )
--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import ReactDom from 'react-dom'
+import { faker } from '@faker-js/faker'
 import CommentDetail from './CommentDetail'
+import ApprovalCard from './ApprovalCard'
 
 const App = () => {
   return (
     <div className='ui container comments'>
-      <CommentDetail author='Sam' />
-      <CommentDetail author='Alex' />
-      <CommentDetail author='Jane' />
+      <ApprovalCard />
+      <CommentDetail
+        author='Sam'
+        timeAgo='Today at 1645'
+        text='Who am I? Sam I am!!!'
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author='Alex'
+        timeAgo='Today at 0200'
+        text='I am King Alexander the Great!'
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail
+        author='Jane'
+        timeAgo='Yesterday at 1700'
+        text='Jane Goodall here reporting live from Australia...'
+        avatar={faker.image.avatar()}
+      />
     </div>
   )
 }
