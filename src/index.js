c2da8abdefac85102a4553b1e6b3fb4862a59f91
ancs214 <ancs214@gmail.com>
2022-07-27 10:04:11 -0400

    passed in comments as props to approvalcard and added nice styling

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import ReactDom from 'react-dom'
+import './style/App.css'
 import { faker } from '@faker-js/faker'
 import CommentDetail from './CommentDetail'
 import ApprovalCard from './ApprovalCard'
@@ -6,25 +7,39 @@ import ApprovalCard from './ApprovalCard'
 const App = () => {
   return (
     <div className='ui container comments'>
-      <ApprovalCard />
-      <CommentDetail
-        author='Sam'
-        timeAgo='Today at 1645'
-        text='Who am I? Sam I am!!!'
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author='Alex'
-        timeAgo='Today at 0200'
-        text='I am King Alexander the Great!'
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author='Jane'
-        timeAgo='Yesterday at 1700'
-        text='Jane Goodall here reporting live from Australia...'
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+
+      {/* this is actually using commentdetail as a prop, so we must pass it into approvalcard as so */}
+      <ApprovalCard>
+        <CommentDetail
+          author='Sam'
+          timeAgo='Today at 1645'
+          text='Who am I? Sam I am!!!'
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author='Alex'
+          timeAgo='Today at 0200'
+          text='I am King Alexander the Great!'
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author='Jane'
+          timeAgo='Yesterday at 1700'
+          text='Jane Goodall here reporting live from Australia...'
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   )
 }
